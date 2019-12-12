cf93d56ad387c793a8fa10267321e0b459ece697
Kevin Tou <toukevin@gmail.com>
2019-12-12 14:19:13 -0800

    Solution using itertools

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python
 
 import sys
+import itertools
+
 
 def rock_paper_scissors(n):
-  pass 
+    moves = ['rock', 'paper', 'scissors']
+    outcomes = []
+    perm = itertools.product(moves, repeat=n)
+    # n is the number of rounds
+    for ways in perm:
+        outcomes.append(list(ways))
+    return outcomes
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
