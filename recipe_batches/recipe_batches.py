cf9c5eba503b34649de348ebd4e63087482ba1fb
Kevin Tou <toukevin@gmail.com>
2019-12-11 17:02:09 -0800

    Re-factored recipe function

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,43 +3,62 @@
 import math
 
 
+# def recipe_batches(recipe, ingredients):
+#     # Recipe is {}
+#     # Ingredients is {}
+
+#     # Verify you have all the ingredients
+#     count = 0
+
+#     for key in recipe:
+#         if key in ingredients:
+#             count += 1
+
+#     batches = 0
+#     numOfBatches = [0] * count
+
+#     # If you have all the ingredients
+#     index = 0
+#     if count == len(recipe.keys()):
+#         # Get the number of batches per ingredient
+#         for key, value in recipe.items():
+#             numOfBatches[index] = ingredients[key] // recipe[key]
+#             index += 1
+#         # print("Batches: ", numOfBatches)
+#         x = 0
+#         while True:
+#             if x == count:
+#                 batches += 1
+#                 x = 0
+#             if numOfBatches[x] == 0:
+#                 break
+#             else:
+#                 numOfBatches[x] -= 1
+#                 x += 1
+#             # print(numOfBatches)
+#         return batches
+#     # If you're missing ingredients, the number of batches is 0
+#     else:
+#         return batches
+
 def recipe_batches(recipe, ingredients):
     # Recipe is {}
     # Ingredients is {}
 
     # Verify you have all the ingredients
-    count = 0
 
     for key in recipe:
-        if key in ingredients:
-            count += 1
-
-    batches = 0
-    numOfBatches = [0] * count
-
-    # If you have all the ingredients
-    index = 0
-    if count == len(recipe.keys()):
-        # Get the number of batches per ingredient
-        for key, value in recipe.items():
-            numOfBatches[index] = ingredients[key] // recipe[key]
-            index += 1
-        # print("Batches: ", numOfBatches)
-        x = 0
-        while True:
-            if x == count:
-                batches += 1
-                x = 0
-            if numOfBatches[x] == 0:
-                break
-            else:
-                numOfBatches[x] -= 1
-                x += 1
-            # print(numOfBatches)
-        return batches
-    # If you're missing ingredients, the number of batches is 0
-    else:
-        return batches
+        if key not in ingredients or len(recipe) > len(ingredients):
+            return 0
+        else:
+            batches = [0] * len(recipe)
+            index = 0
+            # Get the number of batches per ingredient
+            for key, value in recipe.items():
+                batches[index] = ingredients[key] // recipe[key]
+                index += 1
+            # print("Batches: ", numOfBatches)
+            return min(batches)
 
 
 print(recipe_batches({'milk': 100, 'butter': 50, 'flour': 5},
